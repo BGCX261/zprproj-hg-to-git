4c19ca6ed52deff7cdc6f5d1dac8fdc70e2beba4
Rafał Witowski <panwitos@gmail.com>
2010-11-09 21:28:18 +0100

    Django server: further changes in traveler.models. Implemented start point and finish point for traveler (in CityList model)

--- server/traveler/models.py
+++ server/traveler/models.py
@@ -21,63 +21,69 @@ class City(models.Model):
 
 class CityList(models.Model):  
     """describes list of the cities, which have to be visited by traveler"""    
-    name = models.CharField(max_length = 50, unique=True)
+    name        = models.CharField(max_length = 50, unique=True)
     
     #one list can have multiple cities and every city can by included
-    #in many lists. Relation described by 'CityInList' model
-    cities = models.ManyToManyField(City, through='CityInList')
+    #in many lists.
+    cities     = models.ManyToManyField(City)
+    firstCity  = models.ForeignKey(City, related_name = 'firstCity', null=True)
+    lastCity   = models.ForeignKey(City, related_name = 'lastCity', null=True)
+    
     
     def __unicode__(self):
         return self.name
-    
-    
 
-    def addCity(self, city):
-        """add previously created city to the list"""
-        cil = CityInList(city = city, cityList = self)
-        cil.save()
         
-    
+    def addCity(self, city):
+        """add previously created city to the list
+        
+        If list was empty this city is automaticaly set as first city
+        (start point for traveler)
+        """
+        self.cities.add(city)
+        if self.firstCity is None:
+            self.firstCity = city
+       
+            
     def delCity(self, city):
         """remove city from the list"""
-        cil = CityInList.objects.get(city = city, cityList = self)
-        cil.delete()
+        self.cities.get(city).delete()
+
         
     def getCities(self):
-        """get all cities belonging to this city list
+        """get all cities belonging to this city list"""     
+        return self.cities.all()
         
-        cities returned are sorted by the position field, which means the first
-        city in returned list will be a city marked as start point for traveler
-        and the last one will be city marked as traveler's destination point
-        """
         
-        cities = CityInList.objects.order_by('position').filter(cityList = self)
-        return cities
-
-    
-    
-
-class CityInList(models.Model):
-    """describes cities in lists memberships"""
-    
-    #the naming here (exactly the real, short names) is important since it allows
-    #to easily sort some group of cities in order where first city is 'really'
-    #first at the list, and last city is 'really' last one
-    POSITION_CHOICES = (
-        ('1', 'first'),
-        ('2', 'middle'),        
-        ('3', 'last'),
-    )
-    
-    #we have to explicitly tell about foreign keys we will use
-    city = models.ForeignKey(City)
-    cityList = models.ForeignKey(CityList)
-    
-    #whether we should threat city as start / finish point in list or just 
-    #a city that can be somewhere in the middle of any path
-    position = models.CharField(max_length=1, choices=POSITION_CHOICES, default='middle')
-    
-    def __unicode__(self):
-        return self.city.name + ' in ' + self.cityList.name
+    @property
+    def first(self):
+        """start point for traveler"""
+        return self.firstCity
+       
+        
+    @first.setter
+    def first(self, city):
+        self.firstCity = city
+       
+        
+    @property
+    def last(self):
+        """destination point for traveler"""
+        if self.lastCity is not None:
+            return self.lastCity
+        else:
+            #if last city was not set assume that start point is also a finish
+            #point
+            return self.firstCity
+       
+            
+    @last.setter
+    def last(self, city):
+        self.lastCity = city
+        
+        
+    def empty(self):
+        """checks if list is empty"""
+        return self.cities.count() == 0
 
 
