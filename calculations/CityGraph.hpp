1b865857da40308ac98794e12ea04e837bc8f2c2
Rafał Witowski <panwitos@gmail.com>
2010-12-12 21:37:37 +0100

    Calculations part: now using tsp_approx heurisic algorithm from BGL

--- calculations/CityGraph.hpp
+++ calculations/CityGraph.hpp
@@ -1,42 +1,103 @@
+#ifndef CITYGRAPH_HPP
+#define CITYGRAPH_HPP
+
+#include <utility>
+#include <vector>
+#include <iterator>
+
+#include <boost/shared_ptr.hpp>
 #include <boost/graph/adjacency_matrix.hpp>
-#include <map>
+#include <boost/graph/metric_tsp_approx.hpp>
 
 #include "CityList.hpp"
 
 using namespace boost;
+using namespace std;
+
+typedef boost::shared_ptr<CityList> PCityList;
 
 class CityGraph
 {
     public:
-        typedef double Distance;
+        CityGraph(const CityList &city_list) :
+            graph_(city_list.getSize()),
+            origList_(city_list),
+            builded_(false)
+        {}
         
-        CityGraph(int n) : g_(n) {}
-        
-        CityGraph(const CityList &list) : g_(list.getSize())
+        void optimize()
         {
-            
+            if(!builded_)
+            {
+                buildGraph();
+                builded_ = true;
+            }
+                
+            tsp_solve();
         }
         
-        Distance getDistance(int city1_id, int city2_id) const
-        {        
-            property_map<Graph, edge_weight_t>::const_type index = get(edge_weight, g_);
-            graph_traits<Graph>::edge_descriptor e = edge(city1_id, city2_id, g_).first;        
-
-            return get(index, e);
-        }
-        
-        void addEdge(int city1_id, int city2_id, Distance dist)
-        {
-            add_edge(city1_id, city2_id, EdgeProperty(dist), g_);
-        }        
     
     private:
-
-        typedef property<edge_weight_t, Distance> EdgeProperty;
+    
+        typedef property<edge_weight_t, double> EdgeProperty;
         typedef adjacency_matrix<undirectedS, no_property, EdgeProperty> Graph;
         
-        Graph g_;         
+        typedef graph_traits<Graph>::vertex_descriptor Vertex;
+        typedef graph_traits<Graph>::edge_descriptor Edge;  
+        
+        typedef property_map<Graph, vertex_index_t>::type VertexMap; 
+        typedef property_map<Graph, edge_weight_t>::type WeightMap;  
+        
+        PCityList tsp_solve()
+        {
+            typedef vector<Vertex> Container;
+            Container c;
+            metric_tsp_approx_tour(graph_, back_inserter(c));
+            
+            PCityList optimized_list(new CityList);
+            for (Container::iterator ci = c.begin(); ci != c.end(); ++ci)
+            {
+               optimized_list->addCity(origList_.getCities()[*ci]);
+            }
+
+            return optimized_list;
+        }
+        
+        
+        void buildGraph()
+        {
+            typedef typename graph_traits<Graph>::vertex_iterator VItr;                          
+            
+            WeightMap wmap = get(edge_weight, graph_);
+            VertexMap vmap = get(vertex_index, graph_);
+            
+            Edge e;
+            bool inserted;
+
+            pair<VItr, VItr> verts = vertices(graph_);
+            for (VItr src = verts.first; src != verts.second; ++src)
+            {
+                for (VItr dst(src); dst != verts.second; ++dst)
+                {
+                    if (dst != src)
+                    {                                
+                        PCity src_city = origList_.getCities()[vmap[*src]];
+                        PCity dst_city = origList_.getCities()[vmap[*dst]];
+                        
+                        tie(e, inserted) = add_edge(*src, *dst, graph_);
+                        wmap[e] = src_city->distance(*dst_city);
+                    }
+
+                }
+
+            }
+        }
+         
+        
+        Graph graph_;
+        const CityList &origList_;  
+        bool builded_;       
         
 };
 
-
+#endif //CITYGRAPH_HPP
